1550b74d2691b5ab298f040dd54dfec4cf7063e5
AbdulqosimAzizovich <atan60896@gmail.com>
2023-05-15 10:17:51 +0500

    lesson 19

--- 1.py
+++ 1.py
@@ -1,3 +1,30 @@
-a = int(input("Birthday year: "))
-age = 2023 - a
-print(age)
+from sympy import symbols, diff, solve
+from sympy import Symbol
+
+def is_real(expr):
+    return expr.is_real
+
+# Variablarni aniqlash
+x, y = symbols('x y')
+
+# Funksiyani aniqlash
+z = x**2 + x*y + y**2 + 9*x*x - 6*y + 20
+
+# Gradientni topish
+z_x = diff(z, x)
+z_y = diff(z, y)
+
+# Tenglamaga tenglashtirish
+eq1 = z_x
+eq2 = z_y
+eq3 = y + 20*x
+
+# Noto'g'ri koordinatalar
+solutions = solve((eq1, eq2, eq3), (x, y))
+for sol in solutions:
+    if sol[0].is_real and sol[1].is_real:
+        print(f"({sol[0]}, {sol[1]})")
+
+# Minimum qiymat
+z_min = z.subs({x: -1/13, y: 20/13})
+print(f"Minimum qiymat: {z_min}")
